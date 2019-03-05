5f064641492fdf5f66b5ea288df208ac1b091326
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-03-05 09:34:49 -0500

    3.7 JavaScript Beginnings: Starting Our Blackjack Application

---
+++ Blackjack/script.js
@@ -0,0 +1,14 @@
+//
+// Blackjack
+// by mark Zamoyta
+//
+
+let card1 = "Ace of Spades",
+  card2 = "Ten of Hearts";
+  
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log("    " + card1);
+console.log("    " + card2);
+ 
