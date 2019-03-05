4212998b286a1cbbe73d202f37e3dee7edd18d95
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-03-05 09:21:13 -0500

    3.6 JavaScript Beginnings: Commenting Code

---
+++ JSBasics/script.js
@@ -0,0 +1,35 @@
+let productName = "Hammer", productId = "H123";
+console.log(productName, productId);
+
+productName = 'John\'s Hammer';
+console.log(productName);
+
+let price = 5.00, price2 = 3.00;
+console.log(price + price2);
+
+let firstName = 'Joe', lastName = 'Doe';
+console.log(firstName + ' ' + lastName);
+
+let first = 12, second = 3;
+let answer = first + second;
+console.log(answer);
+answer = first - second;
+console.log(answer);
+answer = first * second;
+console.log(answer);
+answer = first / second;
+console.log(answer);
+answer = first % second;
+console.log(answer);
+answer = 3 + 4 * 5;
+console.log(answer);
+answer = (3 + 4) * 5;
+console.log(answer); // prints the answer
+
+//
+// This is a comment
+//
+
+/*
+  This is a comment
+*/
